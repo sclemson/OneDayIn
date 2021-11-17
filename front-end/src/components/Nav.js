423032379645f82b7cdd2cfac76a3fd5746279d3
sclemson <simon_clemson1@hotmail.com>
2021-11-17 10:30:55 +0000

    simonweds

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect, useRef } from 'react'
+import { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { removeToken, removeUserId, getUserId } from '../helpers/auth'
 import { Squash as Hamburger } from 'hamburger-react'
@@ -9,23 +9,8 @@ const Nav = ({ isLoggedIn, setIsLoggedIn }) => {
   const history = useHistory()
 
   const [show, setShow] = useState(false)
-  const draweRef = useRef(null)
-  useEffect(() => {
-    const closeDrawer = (event) => {
-      // console.log(draweRef)
-      console.log(event.target)
-      // if (draweRef.current) {
-      //   return
-      // }
-      // show ? setShow(false) : ''
-    }
-    document.addEventListener('mousedown', closeDrawer)
-    return () => document.removeEventListener('mousedown', closeDrawer)
-  }, [])
 
-  const handleClick = () => {
-    setShow(false)
-  }
+  const handleClose = () => setShow(false)
 
   const handleLogout = () => {
     removeToken()
@@ -40,49 +25,48 @@ const Nav = ({ isLoggedIn, setIsLoggedIn }) => {
       scroll: false,
       backdrop: true
     }
+    // {
+    //   name: 'Enable backdrop (default)',
+    //   scroll: true,
+    //   backdrop: false
+    // }
   ]
 
   return (
     <nav>
-      <Hamburger onClick={() => setShow(true)} { ...options } />
-      <div show={show} ></div>
-      <Offcanvas ref={draweRef} show={show} >
-        <div >
-          <ul>
-            <li>
-              <Link onClick={handleClick} to='/'>Home</Link>
-            </li>
-            <li>
-              <Link onClick={handleClick} to='/about'>About</Link>
-            </li>
-            <li>
-              <Link onClick={handleClick} to='/cities'>Cities</Link>
-            </li>
-          </ul >  
-        </div>
-        {isLoggedIn ? (
-          <>
-            <ul>
+      <Hamburger toggled={show} toggle={setShow} { ...options } />
+      <Offcanvas show={show}>
+        <h2 className='navHeader'>One Day In...</h2>
+        <ul>
+          <li>
+            <Link onClick={handleClose} to='/'>Home</Link>
+          </li>
+          <li>
+            <Link onClick={handleClose} to='/about'>About</Link>
+          </li>
+          <li>
+            <Link onClick={handleClose} to='/cities'>Cities</Link>
+          </li>
+          {isLoggedIn ? (
+            <>
               <li>
-                <Link onClick={handleClick} to={`/users/${getUserId()}`}>Profile</Link>
+                <Link onClick={handleClose} to={`/users/${getUserId()}`}>Profile</Link>
               </li>
               <li>
                 <button onClick={handleLogout}>Logout</button>
               </li>
-            </ul >
-          </>
-        ) : (
-          <>
-            <ul>
+            </>
+          ) : (
+            <>
               <li>
-                <Link to='/register'>Register</Link>
+                <Link onClick={handleClose} to='/register'>Register</Link>
               </li>
               <li>
-                <Link to='/login'>Login</Link>
+                <Link onClick={handleClose} to='/login'>Login</Link>
               </li>
-            </ul >
-          </>
-        )}
+            </>
+          )}
+        </ul>
       </Offcanvas >
     </nav>
   )
